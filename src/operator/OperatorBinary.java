b3e3d2235a3700452d56e6276af8de3c61d07c9f
bijarmi <63535690+bijarmi@users.noreply.github.com>
2020-04-12 16:08:11 +0800

    Add files via upload

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class OperatorBinary {
+    String nama = "Ahmad Bijarmi";
+    int JumlahSks = 24;
+    double IndeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9 + 3);
+        System.out.println(9 - 3);
+        System.out.println(9 * 3);
+        System.out.println(9 / 3);
+        System.out.println(9 % 3);
+        System.out.println(9 % 2);
+    }
+}
